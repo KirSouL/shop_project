35dc35e5de423951417ee1cb02e7146c63284b02
kss <kss@example.com>
2024-09-15 16:20:03 +0400

    update key files dir catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
+from catalog.views import home, contacts, product, category
 
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('category/', category, name='category'),
+    path('product/<int:pk>', product, name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
+from catalog.models import Product, Category
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Shop Market',
+        'title_info': 'Shop Market - это магазин сервисов. Пользуйтесь и получайте наслаждение.'
+
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def category(request):
+    category_list = Category.objects.all()
+    context = {
+        'object_list': category_list,
+        'title': 'Категории продуктов',
+    }
+    return render(request, 'catalog/category.html', context)
+
+
+def product(request, pk):
+    product_list = Product.objects.get(pk=pk)
+    context = {
+        'object_list': product_list,
+        'title': 'Продукт',
+    }
+    return render(request, 'catalog/product.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'Контакты',
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
@@ -15,4 +44,4 @@ def contacts(request):
               f"Почта пользователя: {email},\n"
               f"Телефон пользователя: {phone},\n"
               f"Сообщение от пользователя: {message}")
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
